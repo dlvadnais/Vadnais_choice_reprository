b81f7daf41816380b045dae262fc3c0a3cbbeb6b
dlvadnais <129636431+dlvadnais@users.noreply.github.com>
2024-02-29 15:46:39 -0500

    Add files via upload

---
+++ phptest.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p> i am a human<?php echo" who is real"; ?> </p>  
+    <?php 
+    if (true){
+    echo "hello world";
+    }
+    ?>
+
+    <?php 
+    $dog = "doug dimadome";
+    echo $dog
+    ?>
+
+
+</body>
+</html>
